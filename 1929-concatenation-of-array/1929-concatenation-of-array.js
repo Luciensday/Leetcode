504f1c4cff65099cd578d663f3c041ab79cba921
Lucien Lin <128807685+Luciensday@users.noreply.github.com>
2024-04-02 22:08:32 +0100

    Time: 53 ms (97.38%), Space: 52.8 MB (8.55%) - LeetHub

---
+++ 1929-concatenation-of-array/1929-concatenation-of-array.js
@@ -0,0 +1,17 @@
+/**
+ * @param {number[]} nums
+ * @return {number[]}
+ */
+var getConcatenation = function(nums) {
+ 
+    let ans = nums.slice()
+    
+    for (let i=0; i < nums.length; i++){
+        ans.push(nums[i])
+    }
+    
+    return ans
+  
+    
+    
+};
